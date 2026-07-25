2e38b1b31dfb17ec57b4e43c6c11a1023e54b9a2
Prashant Shukla <prashant93090@gmail.com>
2026-07-25 23:58:38 +0530

    UI UPDATE

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://healthcare-ai-assistant-2.onrender.com",
+  baseURL: "https://healthcare-ai-assistant-4.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
